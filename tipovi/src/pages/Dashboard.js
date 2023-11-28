9928a6c77316ea41dd13bddf4f6a620dc85febf2
iamalexja <aleksandra.bgd.87@gmail.com>
2023-11-28 11:09:55 +0100

    Added dashboard page

---
+++ tipovi/src/pages/Dashboard.js
@@ -0,0 +1,102 @@
+import React, { useEffect } from "react";
+import {
+  MDBContainer,
+  MDBRow,
+  MDBTypography,
+  MDBBtn,
+  MDBIcon,
+  MDBCard,
+  MDBCardHeader,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardFooter,
+} from "mdb-react-ui-kit";
+import { useDispatch, useSelector } from "react-redux";
+import { getTips, deleteTip } from "../redux/features/tipSlice";
+import TipCard from "../components/TipCard";
+import { toast } from "react-toastify";
+import moment from "moment";
+
+const Dashboard = () => {
+  const { tips } = useSelector((state) => ({ ...state.tip }));
+  const { user } = useSelector((state) => ({ ...state.auth }));
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getTips());
+  }, []);
+
+  const handleDelete = (id) => {
+    if (window.confirm("Are you sure you want to delete this tour ?")) {
+      dispatch(deleteTip({ id, toast }));
+    }
+  };
+  return (
+    <div
+      style={{
+        margin: "auto",
+        padding: "15px",
+        maxWidth: "1000px",
+        alignContent: "center",
+      }}
+    >
+      <>
+        <MDBRow className="mt-5">
+          <MDBTypography className="text-center mb-0" tag="h2">
+            Dashboard: {user?.result?.name}
+          </MDBTypography>
+        </MDBRow>
+
+        {tips.length === 0 ? (
+          <MDBTypography className="text-center mb-0" tag="h2">
+            No Tips Found
+          </MDBTypography>
+        ) : (
+          <MDBContainer>
+            <MDBRow className="row-cols-1 row-cols-md-2 g-2">
+              {tips.map((item, index) => (
+                <div key={index}>
+                  <MDBCard alignment="center">
+                    <MDBCardHeader>
+                      <h3>{item.title}</h3>
+                      <span>
+                        <p className="text-start tourName">
+                          Created By: {item.sport}
+                        </p>
+                      </span>
+
+                      <br />
+                    </MDBCardHeader>
+                    <MDBBtn
+                      className="mt-1"
+                      tag="a"
+                      color="none"
+                      onClick={() => handleDelete(item._id)}
+                      style={{ width: "30px", height: "30px" }}
+                    >
+                      <MDBIcon
+                        fas
+                        icon="trash"
+                        style={{ color: "#dd4b39", fontSize: "24px" }}
+                      />
+                    </MDBBtn>
+                    <MDBCardBody>
+                      <MDBCardTitle>{item.rivales}</MDBCardTitle>
+                      <MDBCardText>{item.description}</MDBCardText>
+                    </MDBCardBody>
+                    <MDBCardFooter className="text-muted">{`Tip date: ${moment(
+                      item.tipDate
+                    ).format("DD.MM.YYYY")}`}</MDBCardFooter>
+                  </MDBCard>
+                </div>
+              ))}
+            </MDBRow>
+          </MDBContainer>
+        )}
+      </>
+    </div>
+  );
+};
+
+export default Dashboard;
